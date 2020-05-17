de1215a50bda584af2fedb20670795cfa8495f05
Jimmy Millard <millardjimmy@gmail.com>
2020-05-17 15:55:45 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,6 +1,12 @@
 class Anagram
   attr_accessor :word 
+  def initialize(word)
     @word = word 
   end
   
-  def match
+  def match(w_array)
+    w_array.select do |word|
+      word.split("").sort == @word.split("").sort
+    end
+  end
+end
